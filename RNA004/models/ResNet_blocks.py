a50a0f207a4b0eefa126677edbac1422fc484678
ZJRen <31991009+ZJRen9@users.noreply.github.com>
2025-08-02 17:49:27 +0800

    Add files via upload

---
+++ RNA004/models/ResNet_blocks.py
@@ -0,0 +1,68 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.autograd import Variable
+
+class Res2dNet_mismatch(nn.Module):
+	def __init__(self,dropout_probility,d_model,number_of_classes):
+		super(Res2dNet_mismatch,self).__init__()
+		#################################################
+		self.sequence_conv = nn.Sequential(
+			nn.Conv1d(in_channels=10,out_channels=2,kernel_size=9,padding=0,bias=False),
+			nn.Tanh()
+			)
+		#################################################
+		self.conv_layer_1 = nn.Sequential(
+			nn.Conv2d(in_channels=1,out_channels=d_model,kernel_size=3,padding=1,bias=False),
+			nn.BatchNorm2d(d_model),
+			nn.Tanh(),
+			nn.Dropout(p=dropout_probility,inplace=False)
+			)
+		self.conv_layer_2 = nn.Sequential(
+			nn.Conv2d(in_channels=d_model,out_channels=d_model*4,kernel_size=3,padding=1,bias=False),
+			nn.BatchNorm2d(d_model*4),
+			nn.Tanh(),
+			nn.Dropout(p=dropout_probility,inplace=False)
+			)
+		self.conv_layer_3 = nn.Sequential(
+			nn.Conv2d(in_channels=d_model*4,out_channels=d_model,kernel_size=3,padding=1,bias=False),
+			nn.BatchNorm2d(d_model),
+			nn.Tanh(),
+			nn.Dropout(p=dropout_probility,inplace=False)
+			)
+		self.global_pooling = nn.AdaptiveMaxPool2d(1)
+		self.full_connect = nn.Sequential(
+			nn.Linear(d_model,16),
+			nn.ReLU(),
+			nn.Linear(16,number_of_classes),
+			nn.Sigmoid()
+			)
+
+	def forward(self,sequence_batch,signal_batch):
+		embeding_sequence_batch = self.sequence_conv(sequence_batch)
+		###############
+		embeding_sequence_batch = embeding_sequence_batch.view(embeding_sequence_batch.size()[0],1,embeding_sequence_batch.size()[1],embeding_sequence_batch.size()[2])
+		signal_batch = signal_batch.view(signal_batch.size()[0],1,signal_batch.size()[1],signal_batch.size()[2])
+		x = torch.concat((embeding_sequence_batch,signal_batch),dim=2)
+		###############
+		res1 = self.conv_layer_1(x)
+		res2 = self.conv_layer_2(res1)
+		res3 = self.conv_layer_3(res2)
+		out = res1 + res3
+		out = self.global_pooling(out)
+		out = out.reshape(out.size(0),-1)
+		y_read = self.full_connect(out)
+		return y_read
+
+
+if __name__=="__main__":
+	sequence = torch.zeros((255,10,11))
+	signal = torch.zeros((255,10,3))
+	model = Res2dNet_mismatch(dropout_probility=0.2,d_model=32,number_of_classes=3)
+	out = model(
+		sequence_batch=sequence,
+		signal_batch=signal
+		)
+	print(out.shape)
---
+++ RNA004/models/__init__.py
@@ -0,0 +1,8 @@
+
+from .m6Aiso_model import m6AisoNet
+from .ResNet import multi_modification_Net
+
+__all__ = [
+	'm6AisoNet',
+	'multi_modification_Net'
+	]
